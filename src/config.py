a915582a4a258347a262dbeff1becd26f2b5e621
xxjan719 <xingjianxu@ufl.edu>
2026-03-19 15:20:37 -0400

    update to equipart

--- src/config.py
+++ src/config.py
@@ -234,7 +234,7 @@ class Config:
         # Case selection
         parser.add_argument('--params_name', type=str, 
                             choices = ['cascade', 'equipart','dual_cascade','periodic_cascade','random_cascade'],
-                            default='dual_cascade',
+                            default='equipart',
                             help='Case to use')
         # Seed
         parser.add_argument('--SEED', type=int, 
